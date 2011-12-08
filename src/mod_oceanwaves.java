1350acbda16ec68259a97f28023b92e73c00ad77
riking <riking28@gmail.com>
2011-12-08 07:56:17 -0800

    added getHeight()

--- src/mod_oceanwaves.java
+++ src/mod_oceanwaves.java
@@ -1,4 +1,9 @@
 public class mod_oceanwaves extends BaseMod
 {
 	public String Version() { "Minecraft 1.0.0 mod v0"; }
+	private float[] heights = {0.2f,0.3f,0.5f,0.8f,1.0f,1.1f,0.9f,0.3f};
+	public float getHeight(ry world, int posx, int posy, int posz, Random rand)
+	{
+		return heights[((world.time % 8)+posx) % 8];
+	}
 }
