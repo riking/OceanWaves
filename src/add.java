193420c6c835b197882cc03e7e62ec6908427a12
Riking <rikingcoding@gmail.com>
2011-12-07 19:52:49 -0800

    Fixed indents & some style issues

--- src/add.java
+++ src/add.java
@@ -2,53 +2,57 @@ import java.util.Random;
 
 public class add extends agw
 {
-  protected add(int paramInt, p paramp)
-  {
-    super(paramInt, paramp);
+	protected add(int paramInt, p paramp)
+	{
+		super(paramInt, paramp);
 
-    b(false);
-    if (paramp == p.h) b(true);
-  }
+		b(false);
+		if (paramp == p.h) b(true);
+	}
 
-  public void a(ry paramry, int paramInt1, int paramInt2, int paramInt3, int paramInt4)
-  {
-    super.a(paramry, paramInt1, paramInt2, paramInt3, paramInt4);
-    if (paramry.a(paramInt1, paramInt2, paramInt3) == this.bM)
-      j(paramry, paramInt1, paramInt2, paramInt3);
-  }
+	public void a(ry paramry, int paramInt1, int paramInt2, int paramInt3, int paramInt4)
+	{
+		super.a(paramry, paramInt1, paramInt2, paramInt3, paramInt4);
+		if (paramry.a(paramInt1, paramInt2, paramInt3) == this.bM)
+			j(paramry, paramInt1, paramInt2, paramInt3);
+	}
 
-  private void j(ry paramry, int paramInt1, int paramInt2, int paramInt3)
-  {
-    int i = paramry.d(paramInt1, paramInt2, paramInt3);
-    paramry.t = true;
-    paramry.b(paramInt1, paramInt2, paramInt3, this.bM - 1, i);
-    paramry.c(paramInt1, paramInt2, paramInt3, paramInt1, paramInt2, paramInt3);
-    paramry.a(paramInt1, paramInt2, paramInt3, this.bM - 1, d());
-    paramry.t = false;
-  }
+	private void j(ry paramry, int paramInt1, int paramInt2, int paramInt3)
+	{
+		int i = paramry.d(paramInt1, paramInt2, paramInt3);
+		paramry.t = true;
+		paramry.b(paramInt1, paramInt2, paramInt3, this.bM - 1, i);
+		paramry.c(paramInt1, paramInt2, paramInt3, paramInt1, paramInt2, paramInt3);
+		paramry.a(paramInt1, paramInt2, paramInt3, this.bM - 1, d());
+		paramry.t = false;
+	}
 
-  public void a(ry paramry, int paramInt1, int paramInt2, int paramInt3, Random paramRandom) {
-    if (this.bZ == p.h) {
-      int i = paramRandom.nextInt(3);
-      for (int j = 0; j < i; j++) {
-        paramInt1 += paramRandom.nextInt(3) - 1;
-        paramInt2++;
-        paramInt3 += paramRandom.nextInt(3) - 1;
-        int k = paramry.a(paramInt1, paramInt2, paramInt3);
-        if (k == 0) {
-          if ((!k(paramry, paramInt1 - 1, paramInt2, paramInt3)) && (!k(paramry, paramInt1 + 1, paramInt2, paramInt3)) && (!k(paramry, paramInt1, paramInt2, paramInt3 - 1)) && (!k(paramry, paramInt1, paramInt2, paramInt3 + 1)) && (!k(paramry, paramInt1, paramInt2 - 1, paramInt3)) && (!k(paramry, paramInt1, paramInt2 + 1, paramInt3)))
-            continue;
-          paramry.g(paramInt1, paramInt2, paramInt3, yy.ar.bM);
-          return;
-        }
-        if (yy.k[k].bZ.d())
-          return;
-      }
-    }
-  }
+	public void a(ry paramry, int paramInt1, int paramInt2, int paramInt3, Random paramRandom)
+	{
+		if (this.bZ == p.h)
+		{
+			int i = paramRandom.nextInt(3);
+			for (int j = 0; j < i; j++)
+			{
+				paramInt1 += paramRandom.nextInt(3) - 1;
+				paramInt2++;
+				paramInt3 += paramRandom.nextInt(3) - 1;
+				int k = paramry.a(paramInt1, paramInt2, paramInt3);
+				if (k == 0)
+				{
+					if ((!k(paramry, paramInt1 - 1, paramInt2, paramInt3)) && (!k(paramry, paramInt1 + 1, paramInt2, paramInt3)) && (!k(paramry, paramInt1, paramInt2, paramInt3 - 1)) && (!k(paramry, paramInt1, paramInt2, paramInt3 + 1)) && (!k(paramry, paramInt1, paramInt2 - 1, paramInt3)) && (!k(paramry, paramInt1, paramInt2 + 1, paramInt3)))
+							continue;
+					paramry.g(paramInt1, paramInt2, paramInt3, yy.ar.bM);
+					return;
+				}
+				if (yy.k[k].bZ.d())
+					return;
+			}
+		}
+	}
 
-  private boolean k(ry paramry, int paramInt1, int paramInt2, int paramInt3)
-  {
-    return paramry.e(paramInt1, paramInt2, paramInt3).g();
-  }
+	private boolean k(ry paramry, int paramInt1, int paramInt2, int paramInt3)
+	{
+		return paramry.e(paramInt1, paramInt2, paramInt3).g();
+	}
 }
--- src/ahx.java
+++ src/ahx.java
@@ -2,224 +2,274 @@ import java.util.Random;
 
 public class ahx extends agw
 {
-  int a = 0;
+	int a = 0;
 
-  boolean[] cb = new boolean[4];
-  int[] cc = new int[4];
+	boolean[] cb = new boolean[4];
+	int[] cc = new int[4];
 
-  protected ahx(int paramInt, p paramp)
-  {
-    super(paramInt, paramp);
-  }
+	protected ahx(int paramInt, p paramp)
+	{
+		super(paramInt, paramp);
+	}
 
-  private void j(ry paramry, int paramInt1, int paramInt2, int paramInt3) {
-    int i = paramry.d(paramInt1, paramInt2, paramInt3);
+	private void j(ry paramry, int paramInt1, int paramInt2, int paramInt3)
+	{
+		int i = paramry.d(paramInt1, paramInt2, paramInt3);
 
-    paramry.b(paramInt1, paramInt2, paramInt3, this.bM + 1, i);
-    paramry.c(paramInt1, paramInt2, paramInt3, paramInt1, paramInt2, paramInt3);
-    paramry.j(paramInt1, paramInt2, paramInt3);
-  }
+		paramry.b(paramInt1, paramInt2, paramInt3, this.bM + 1, i);
+		paramry.c(paramInt1, paramInt2, paramInt3, paramInt1, paramInt2, paramInt3);
+		paramry.j(paramInt1, paramInt2, paramInt3);
+	}
 
-  public void a(ry paramry, int paramInt1, int paramInt2, int paramInt3, Random paramRandom)
-  {
-    int i = g(paramry, paramInt1, paramInt2, paramInt3);
+	public void a(ry paramry, int paramInt1, int paramInt2, int paramInt3, Random paramRandom)
+	{
+		int i = g(paramry, paramInt1, paramInt2, paramInt3);
 
-    int j = 1;
-    if ((this.bZ == p.h) && (!paramry.y.d)) j = 2;
+		int j = 1;
+		if ((this.bZ == p.h) && (!paramry.y.d))
+		{
+			j = 2;
+		}
+		int k = 1;
+		int n;
+		if (i > 0)
+		{
+			int m = -100;
+			this.a = 0;
+			m = f(paramry, paramInt1 - 1, paramInt2, paramInt3, m);
+			m = f(paramry, paramInt1 + 1, paramInt2, paramInt3, m);
+			m = f(paramry, paramInt1, paramInt2, paramInt3 - 1, m);
+			m = f(paramry, paramInt1, paramInt2, paramInt3 + 1, m);
 
-    int k = 1;
-    int n;
-    if (i > 0) {
-      int m = -100;
-      this.a = 0;
-      m = f(paramry, paramInt1 - 1, paramInt2, paramInt3, m);
-      m = f(paramry, paramInt1 + 1, paramInt2, paramInt3, m);
-      m = f(paramry, paramInt1, paramInt2, paramInt3 - 1, m);
-      m = f(paramry, paramInt1, paramInt2, paramInt3 + 1, m);
+			n = m + j;
+			if ((n >= 8) || (m < 0))
+			{
+				n = -1;
+			}
+			if (g(paramry, paramInt1, paramInt2 + 1, paramInt3) >= 0)
+			{
+				int i1 = g(paramry, paramInt1, paramInt2 + 1, paramInt3);
+				if (i1 >= 8)
+				{
+					n = i1;				
+				}
+				else
+				{
+					n = i1 + 8;
+				}
+			}
+			if ((this.a >= 2) && (this.bZ == p.g))
+			{
+				if (paramry.e(paramInt1, paramInt2 - 1, paramInt3).b())
+				{
+					n = 0;
+				} 
+				else if ((paramry.e(paramInt1, paramInt2 - 1, paramInt3) == this.bZ) && (paramry.d(paramInt1, paramInt2, paramInt3) == 0))
+				{
+					n = 0;
+				}
+			}
+			if ((this.bZ == p.h) && 
+			 (i < 8) && (n < 8) && (n > i) && 
+			 (paramRandom.nextInt(4) != 0))
+			{
+				n = i;
+				k = 0;
+			}
 
-      n = m + j;
-      if ((n >= 8) || (m < 0)) {
-        n = -1;
-      }
-      if (g(paramry, paramInt1, paramInt2 + 1, paramInt3) >= 0) {
-        int i1 = g(paramry, paramInt1, paramInt2 + 1, paramInt3);
-        if (i1 >= 8) n = i1; else
-          n = i1 + 8;
-      }
-      if ((this.a >= 2) && (this.bZ == p.g))
-      {
-        if (paramry.e(paramInt1, paramInt2 - 1, paramInt3).b())
-          n = 0;
-        else if ((paramry.e(paramInt1, paramInt2 - 1, paramInt3) == this.bZ) && (paramry.d(paramInt1, paramInt2, paramInt3) == 0)) {
-          n = 0;
-        }
-      }
-      if ((this.bZ == p.h) && 
-        (i < 8) && (n < 8) && 
-        (n > i) && 
-        (paramRandom.nextInt(4) != 0)) {
-        n = i;
-        k = 0;
-      }
+			if (n != i) {
+				i = n;
+				if (i < 0)
+				{
+					paramry.g(paramInt1, paramInt2, paramInt3, 0);
+				}
+				else
+				{
+					paramry.f(paramInt1, paramInt2, paramInt3, i);
+					paramry.a(paramInt1, paramInt2, paramInt3, this.bM, d());
+					paramry.j(paramInt1, paramInt2, paramInt3, this.bM);
+				}
+			}
+			else if (k != 0)
+			{
+				j(paramry, paramInt1, paramInt2, paramInt3);
+			}
+		}
+		else
+		{
+			j(paramry, paramInt1, paramInt2, paramInt3);
+		}
+		if (m(paramry, paramInt1, paramInt2 - 1, paramInt3))
+		{
+			if ((this.bZ == p.h) && 
+			 (paramry.e(paramInt1, paramInt2 - 1, paramInt3) == p.g))
+			{
+				paramry.g(paramInt1, paramInt2 - 1, paramInt3, yy.t.bM);
+				h(paramry, paramInt1, paramInt2 - 1, paramInt3);
+				return;
+			}
 
-      if (n != i) {
-        i = n;
-        if (i < 0) {
-          paramry.g(paramInt1, paramInt2, paramInt3, 0);
-        } else {
-          paramry.f(paramInt1, paramInt2, paramInt3, i);
-          paramry.a(paramInt1, paramInt2, paramInt3, this.bM, d());
-          paramry.j(paramInt1, paramInt2, paramInt3, this.bM);
-        }
-      }
-      else if (k != 0) { j(paramry, paramInt1, paramInt2, paramInt3); }
-    }
-    else {
-      j(paramry, paramInt1, paramInt2, paramInt3);
-    }
-    if (m(paramry, paramInt1, paramInt2 - 1, paramInt3)) {
-      if ((this.bZ == p.h) && 
-        (paramry.e(paramInt1, paramInt2 - 1, paramInt3) == p.g)) {
-        paramry.g(paramInt1, paramInt2 - 1, paramInt3, yy.t.bM);
-        h(paramry, paramInt1, paramInt2 - 1, paramInt3);
-        return;
-      }
+			if (i >= 8)
+			{
+				paramry.d(paramInt1, paramInt2 - 1, paramInt3, this.bM, i);
+			}
+			else
+			{
+				paramry.d(paramInt1, paramInt2 - 1, paramInt3, this.bM, i + 8);
+			}
+		}
+		else if ((i >= 0) && ((i == 0) || (l(paramry, paramInt1, paramInt2 - 1, paramInt3))))
+		{
+			boolean[] arrayOfBoolean = k(paramry, paramInt1, paramInt2, paramInt3);
+			n = i + j;
+			if (i >= 8)
+			{
+				n = 1;
+			}
+			if (n >= 8) return;
+			if (arrayOfBoolean[0] != 0) g(paramry, paramInt1 - 1, paramInt2, paramInt3, n);
+			if (arrayOfBoolean[1] != 0) g(paramry, paramInt1 + 1, paramInt2, paramInt3, n);
+			if (arrayOfBoolean[2] != 0) g(paramry, paramInt1, paramInt2, paramInt3 - 1, n);
+			if (arrayOfBoolean[3] != 0) g(paramry, paramInt1, paramInt2, paramInt3 + 1, n);
+		}
+	}
 
-      if (i >= 8) paramry.d(paramInt1, paramInt2 - 1, paramInt3, this.bM, i); else
-        paramry.d(paramInt1, paramInt2 - 1, paramInt3, this.bM, i + 8);
-    } else if ((i >= 0) && ((i == 0) || (l(paramry, paramInt1, paramInt2 - 1, paramInt3)))) {
-      boolean[] arrayOfBoolean = k(paramry, paramInt1, paramInt2, paramInt3);
-      n = i + j;
-      if (i >= 8) {
-        n = 1;
-      }
-      if (n >= 8) return;
-      if (arrayOfBoolean[0] != 0) g(paramry, paramInt1 - 1, paramInt2, paramInt3, n);
-      if (arrayOfBoolean[1] != 0) g(paramry, paramInt1 + 1, paramInt2, paramInt3, n);
-      if (arrayOfBoolean[2] != 0) g(paramry, paramInt1, paramInt2, paramInt3 - 1, n);
-      if (arrayOfBoolean[3] != 0) g(paramry, paramInt1, paramInt2, paramInt3 + 1, n);
-    }
-  }
+	private void g(ry paramry, int paramInt1, int paramInt2, int paramInt3, int paramInt4)
+	{
+		if (m(paramry, paramInt1, paramInt2, paramInt3))
+		{
+			int i = paramry.a(paramInt1, paramInt2, paramInt3);
+			if (i > 0)
+			{
+				if (this.bZ == p.h)
+				{
+					h(paramry, paramInt1, paramInt2, paramInt3);
+				} else {
+					yy.k[i].b(paramry, paramInt1, paramInt2, paramInt3, paramry.d(paramInt1, paramInt2, paramInt3), 0);
+				}
+			}
+			paramry.d(paramInt1, paramInt2, paramInt3, this.bM, paramInt4);
+		}
+	}
 
-  private void g(ry paramry, int paramInt1, int paramInt2, int paramInt3, int paramInt4)
-  {
-    if (m(paramry, paramInt1, paramInt2, paramInt3)) {
-      int i = paramry.a(paramInt1, paramInt2, paramInt3);
-      if (i > 0) {
-        if (this.bZ == p.h)
-          h(paramry, paramInt1, paramInt2, paramInt3);
-        else {
-          yy.k[i].b(paramry, paramInt1, paramInt2, paramInt3, paramry.d(paramInt1, paramInt2, paramInt3), 0);
-        }
-      }
-      paramry.d(paramInt1, paramInt2, paramInt3, this.bM, paramInt4);
-    }
-  }
+	private int c(ry paramry, int paramInt1, int paramInt2, int paramInt3, int paramInt4, int paramInt5)
+	{
+		int i = 1000;
+		for (int j = 0; j < 4; j++)
+		{
+			if (((j == 0) && (paramInt5 == 1)) || 
+			 ((j == 1) && (paramInt5 == 0)) || 
+			 ((j == 2) && (paramInt5 == 3)) ||
+			 ((j == 3) && (paramInt5 == 2)))
+			{
+				continue;
+			}
+			int k = paramInt1;
+			int m = paramInt2;
+			int n = paramInt3;
 
-  private int c(ry paramry, int paramInt1, int paramInt2, int paramInt3, int paramInt4, int paramInt5)
-  {
-    int i = 1000;
-    for (int j = 0; j < 4; j++) {
-      if (((j == 0) && (paramInt5 == 1)) || 
-        ((j == 1) && (paramInt5 == 0)) || 
-        ((j == 2) && (paramInt5 == 3)) || (
-        (j == 3) && (paramInt5 == 2)))
-        continue;
-      int k = paramInt1;
-      int m = paramInt2;
-      int n = paramInt3;
+			if (j == 0) k--;
+			if (j == 1) k++;
+			if (j == 2) n--;
+			if (j == 3) n++;
 
-      if (j == 0) k--;
-      if (j == 1) k++;
-      if (j == 2) n--;
-      if (j == 3) n++;
+			if (l(paramry, k, m, n))
+				continue;
+			if ((paramry.e(k, m, n) == this.bZ) && (paramry.d(k, m, n) == 0))
+				continue;
+			if (!l(paramry, k, m - 1, n))
+			{
+				return paramInt4;
+			}
+			if (paramInt4 < 4)
+			{
+				int i1 = c(paramry, k, m, n, paramInt4 + 1, j);
+				if (i1 >= i) continue;
+				i = i1;
+			}
 
-      if (l(paramry, k, m, n))
-        continue;
-      if ((paramry.e(k, m, n) == this.bZ) && (paramry.d(k, m, n) == 0)) {
-        continue;
-      }
-      if (!l(paramry, k, m - 1, n)) {
-        return paramInt4;
-      }
-      if (paramInt4 < 4) {
-        int i1 = c(paramry, k, m, n, paramInt4 + 1, j);
-        if (i1 >= i) continue; i = i1;
-      }
+		}
 
-    }
+		return i;
+	}
 
-    return i;
-  }
+	private boolean[] k(ry paramry, int paramInt1, int paramInt2, int paramInt3)
+	{
+		for (int i = 0; i < 4; i++) {
+			this.cc[i] = 1000;
+			j = paramInt1;
+			int k = paramInt2;
+			int m = paramInt3;
 
-  private boolean[] k(ry paramry, int paramInt1, int paramInt2, int paramInt3)
-  {
-    for (int i = 0; i < 4; i++) {
-      this.cc[i] = 1000;
-      j = paramInt1;
-      int k = paramInt2;
-      int m = paramInt3;
+			if (i == 0) j--;
+			if (i == 1) j++;
+			if (i == 2) m--;
+			if (i == 3) m++;
+			if (l(paramry, j, k, m))
+				continue;
+			if ((paramry.e(j, k, m) == this.bZ) && (paramry.d(j, k, m) == 0))
+				continue;
+			if (!l(paramry, j, k - 1, m))
+			{
+				this.cc[i] = 0;
+			}
+			else
+			{
+				this.cc[i] = c(paramry, j, k, m, 1, i);
+			}
 
-      if (i == 0) j--;
-      if (i == 1) j++;
-      if (i == 2) m--;
-      if (i == 3) m++;
-      if (l(paramry, j, k, m))
-        continue;
-      if ((paramry.e(j, k, m) == this.bZ) && (paramry.d(j, k, m) == 0)) {
-        continue;
-      }
-      if (!l(paramry, j, k - 1, m))
-        this.cc[i] = 0;
-      else {
-        this.cc[i] = c(paramry, j, k, m, 1, i);
-      }
+		}
 
-    }
+		i = this.cc[0];
+		for (int j = 1; j < 4; j++)
+		{
+			if (this.cc[j] >= i) continue;
+			i = this.cc[j];
+		}
 
-    i = this.cc[0];
-    for (int j = 1; j < 4; j++) {
-      if (this.cc[j] >= i) continue; i = this.cc[j];
-    }
+		for (j = 0; j < 4; j++)
+		{
+			this.cb[j] = (this.cc[j] == i ? 1 : false);
+		}
+		return this.cb;
+	}
 
-    for (j = 0; j < 4; j++) {
-      this.cb[j] = (this.cc[j] == i ? 1 : false);
-    }
-    return this.cb;
-  }
+	private boolean l(ry paramry, int paramInt1, int paramInt2, int paramInt3)
+	{
+		int i = paramry.a(paramInt1, paramInt2, paramInt3);
+		if ((i == yy.aE.bM) || (i == yy.aL.bM) || (i == yy.aD.bM) || (i == yy.aF.bM) || (i == yy.aX.bM))
+		{
+			return true;
+		}
+		if (i == 0) return false;
+		p localp = yy.k[i].bZ;
+		if (localp == p.A) return true;
+		return localp.d();
+	}
 
-  private boolean l(ry paramry, int paramInt1, int paramInt2, int paramInt3) {
-    int i = paramry.a(paramInt1, paramInt2, paramInt3);
-    if ((i == yy.aE.bM) || (i == yy.aL.bM) || (i == yy.aD.bM) || (i == yy.aF.bM) || (i == yy.aX.bM)) {
-      return true;
-    }
-    if (i == 0) return false;
-    p localp = yy.k[i].bZ;
-    if (localp == p.A) return true;
-    return localp.d();
-  }
+	protected int f(ry paramry, int paramInt1, int paramInt2, int paramInt3, int paramInt4)
+	{
+		int i = g(paramry, paramInt1, paramInt2, paramInt3);
+		if (i < 0) return paramInt4;
+		if (i == 0) this.a += 1;
+		if (i >= 8) i = 0;
+		return (paramInt4 < 0) || (i < paramInt4) ? i : paramInt4;
+	}
 
-  protected int f(ry paramry, int paramInt1, int paramInt2, int paramInt3, int paramInt4)
-  {
-    int i = g(paramry, paramInt1, paramInt2, paramInt3);
-    if (i < 0) return paramInt4;
-    if (i == 0) this.a += 1;
-    if (i >= 8) {
-      i = 0;
-    }
-    return (paramInt4 < 0) || (i < paramInt4) ? i : paramInt4;
-  }
+	private boolean m(ry paramry, int paramInt1, int paramInt2, int paramInt3)
+	{
+		p localp = paramry.e(paramInt1, paramInt2, paramInt3);
+		if (localp == this.bZ) return false;
+		if (localp == p.h) return false;
+		return !l(paramry, paramInt1, paramInt2, paramInt3);
+	}
 
-  private boolean m(ry paramry, int paramInt1, int paramInt2, int paramInt3) {
-    p localp = paramry.e(paramInt1, paramInt2, paramInt3);
-    if (localp == this.bZ) return false;
-    if (localp == p.h) return false;
-    return !l(paramry, paramInt1, paramInt2, paramInt3);
-  }
-
-  public void a(ry paramry, int paramInt1, int paramInt2, int paramInt3) {
-    super.a(paramry, paramInt1, paramInt2, paramInt3);
-    if (paramry.a(paramInt1, paramInt2, paramInt3) == this.bM)
-      paramry.a(paramInt1, paramInt2, paramInt3, this.bM, d());
-  }
+	public void a(ry paramry, int paramInt1, int paramInt2, int paramInt3)
+	{
+		super.a(paramry, paramInt1, paramInt2, paramInt3);
+		if (paramry.a(paramInt1, paramInt2, paramInt3) == this.bM)
+		{
+			paramry.a(paramInt1, paramInt2, paramInt3, this.bM, d());
+		}
+	}
 }
