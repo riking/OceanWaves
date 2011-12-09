cf6bbd0018f8597e7ee3f8c8521f65cd53571b8a
TehKrush <tehkrush@gmail.com>
2011-12-08 23:33:50 -0500

    fixing ModLoader stuff

--- src/mod_oceanwaves.java
+++ src/mod_oceanwaves.java
@@ -1,9 +1,12 @@
-public class mod_oceanwaves extends BaseMod
-{
-	public String Version() { "Minecraft 1.0.0 mod v0"; }
-	private float[] heights = {0.2f,0.3f,0.5f,0.8f,1.0f,1.1f,0.9f,0.3f};
-	public float getHeight(ry world, int posx, int posy, int posz, Random rand)
-	{
-		return heights[((world.time % 8)+posx) % 8];
-	}
+public class mod_oceanwaves extends BaseMod {
+  public String getVersion() { "Minecraft 1.0.0 mod v0"; }
+  private float[] heights = {0.2f,0.3f,0.5f,0.8f,1.0f,1.1f,0.9f,0.3f};
+
+  public void load() {
+    // this is the BaseMod method to put all your shit in, like how mod_whatever() used to be.
+  }
+
+  public float getHeight(ry world, int posx, int posy, int posz, Random rand) {
+    return heights[((world.time % 8)+posx) % 8];
+  }
 }
